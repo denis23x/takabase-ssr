5c6e5219d031614a4b980e1ec6b8b645c82cb41d
denis23x <damage.23x@gmail.com>
2022-01-10 18:02:10 +0200

    added pluck

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, pluck } from 'rxjs/operators';
 import { SnackbarService } from './snackbar.service';
 import { RequestError, RequestBody, RequestParams } from '../models';
 
@@ -37,26 +37,30 @@ export class ApiService {
   }
 
   get(path: string, requestParams?: RequestParams): Observable<any> {
-    return this.http
-      .get(environment.API_URL + path, { params: requestParams })
-      .pipe(catchError((httpErrorResponse: HttpErrorResponse) => this.setError(httpErrorResponse)));
+    return this.http.get(environment.API_URL + path, { params: requestParams }).pipe(
+      pluck('data'),
+      catchError((httpErrorResponse: HttpErrorResponse) => this.setError(httpErrorResponse))
+    );
   }
 
   put(path: string, requestBody?: RequestBody): Observable<any> {
-    return this.http
-      .put(environment.API_URL + path, JSON.stringify(requestBody || {}))
-      .pipe(catchError((httpErrorResponse: HttpErrorResponse) => this.setError(httpErrorResponse)));
+    return this.http.put(environment.API_URL + path, JSON.stringify(requestBody || {})).pipe(
+      pluck('data'),
+      catchError((httpErrorResponse: HttpErrorResponse) => this.setError(httpErrorResponse))
+    );
   }
 
   post(path: string, requestBody?: RequestBody): Observable<any> {
-    return this.http
-      .post(environment.API_URL + path, JSON.stringify(requestBody || {}))
-      .pipe(catchError((httpErrorResponse: HttpErrorResponse) => this.setError(httpErrorResponse)));
+    return this.http.post(environment.API_URL + path, JSON.stringify(requestBody || {})).pipe(
+      pluck('data'),
+      catchError((httpErrorResponse: HttpErrorResponse) => this.setError(httpErrorResponse))
+    );
   }
 
   delete(path: string): Observable<any> {
-    return this.http
-      .delete(environment.API_URL + path)
-      .pipe(catchError((httpErrorResponse: HttpErrorResponse) => this.setError(httpErrorResponse)));
+    return this.http.delete(environment.API_URL + path).pipe(
+      pluck('data'),
+      catchError((httpErrorResponse: HttpErrorResponse) => this.setError(httpErrorResponse))
+    );
   }
 }
