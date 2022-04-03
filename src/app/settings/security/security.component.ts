485578b78687553470cc1593080f43a9988637d7
denis23x <damage.23x@gmail.com>
2022-04-03 18:04:51 +0200

    update

--- src/app/settings/security/security.component.ts
+++ src/app/settings/security/security.component.ts
@@ -1,10 +1,11 @@
 /** @format */
 
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { pluck } from 'rxjs/operators';
-import { AuthService, Session, User } from '../../core';
+import { pluck, switchMap } from 'rxjs/operators';
+import { AuthService, Session, SnackbarService, User } from '../../core';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
+import { GetResult } from '@fingerprintjs/fingerprintjs';
 
 @Component({
   selector: 'app-settings-security',
@@ -15,39 +16,64 @@ export class SettingsSecurityComponent implements OnInit, OnDestroy {
 
   user: User;
 
+  sessionCurrent!: Session;
+  sessionList: Session[] = [];
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private snackbarService: SnackbarService
   ) {}
 
   ngOnInit(): void {
     this.routeData$ = this.activatedRoute.data
-      .pipe(pluck('data'))
-      .subscribe((user: User) => (this.user = user));
+      .pipe(
+        pluck('data'),
+        switchMap((user: User) => {
+          this.user = user;
+
+          return this.authService.getFingerprint();
+        })
+      )
+      .subscribe((getResult: GetResult) => {
+        console.log(getResult);
+
+        // https://www.npmjs.com/package/ipinfo
+
+        this.sessionCurrent = this.user.sessions.find((session: Session) => {
+          return session.fingerprint === getResult.visitorId;
+        });
+
+        this.sessionList = this.user.sessions.filter((session: Session) => {
+          return session.fingerprint !== getResult.visitorId;
+        });
+      });
   }
 
   ngOnDestroy(): void {
     [this.routeData$].filter($ => $).forEach($ => $.unsubscribe());
   }
 
-  onLogout(session: Session): void {
-    console.log(session);
-    // this.authService
-    //   .onLogout({
-    //     fingerprint: session.fingerprint
-    //   })
-    //   .subscribe(() => this.router.navigateByUrl('/').then(() => console.debug('Route changed')));
-  }
+  onLogout(session?: Session): void {
+    if (!!session) {
+      this.authService
+        .onLogout({
+          id: session.id
+        })
+        .subscribe(() => {
+          this.snackbarService.success('Session removed');
 
-  onLogoutAll(): void {
-    // this.authService
-    //   .onLogout({
-    //     fingerprint: 'any',
-    //     reset: 1
-    //   })
-    //   .subscribe(() => {
-    //     console.log('onLogoutAll');
-    //   });
+          this.sessionList = this.sessionList.filter((session2: Session) => {
+            return session2.id !== session.id;
+          });
+        });
+    } else {
+      this.authService
+        .onLogout({
+          reset: 1
+        })
+        .subscribe(() => this.router.navigateByUrl('/').then(() => console.debug('Route changed')));
+    }
   }
 }
