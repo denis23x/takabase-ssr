561869e5ac48acfffa2c6ebb13a1602f5ebee643
denis23x <damage.23x@gmail.com>
2022-04-17 21:14:54 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent, SharedModule, SnackbarComponent } from './shared';
+import { SharedModule } from './shared';
 import { CoreModule } from './core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SnackbarComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     CoreModule,
---
+++ src/app/shared/components/share/share.component.ts
@@ -0,0 +1,15 @@
+/** @format */
+
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-share',
+  templateUrl: './share.component.html'
+})
+export class ShareComponent implements OnInit, OnDestroy {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {}
+}
