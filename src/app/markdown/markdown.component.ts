278838d92fbb6b52d0521e5f96aade842e3576eb
denis23x <damage.23x@gmail.com>
2022-05-12 17:46:59 +0600

    added drag html

--- src/app/markdown/markdown.component.ts
+++ src/app/markdown/markdown.component.ts
@@ -1,11 +1,20 @@
 /** @format */
 
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import { PlatformService, HelperService, Post } from '../core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { fromEvent, Subscription } from 'rxjs';
 import { filter, pluck } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DOCUMENT } from '@angular/common';
 import Split from 'split-grid';
 
 @Component({
@@ -17,6 +26,10 @@ export class MarkdownComponent implements OnInit, AfterViewInit, OnDestroy {
 
   activatedRouteData$: Subscription;
 
+  mouseup$: Subscription;
+  mousedown$: Subscription;
+  mousemove$: Subscription;
+
   postForm: FormGroup;
   postForm$: Subscription;
 
@@ -25,6 +38,8 @@ export class MarkdownComponent implements OnInit, AfterViewInit, OnDestroy {
   editorScrollSync: boolean;
 
   constructor(
+    @Inject(DOCUMENT)
+    private document: Document,
     private formBuilder: FormBuilder,
     private platformService: PlatformService,
     private activatedRoute: ActivatedRoute,
@@ -47,6 +62,8 @@ export class MarkdownComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngAfterViewInit(): void {
     if (this.platformService.isBrowser()) {
+      /** Initialize Split */
+
       Split({
         minSize: this.editorMinSize,
         columnGutters: [
@@ -56,11 +73,67 @@ export class MarkdownComponent implements OnInit, AfterViewInit, OnDestroy {
           }
         ]
       });
+
+      /** Initialize Drag HTML */
+
+      this.onDrag();
     }
   }
 
   ngOnDestroy(): void {
-    [this.activatedRouteData$, this.postForm$].forEach($ => $?.unsubscribe());
+    [
+      this.activatedRouteData$,
+      this.mouseup$,
+      this.mousedown$,
+      this.mousemove$,
+      this.postForm$
+    ].forEach($ => $?.unsubscribe());
+  }
+
+  onDrag(): void {
+    /** https://htmldom.dev/drag-to-scroll/ */
+
+    const htmlElement: HTMLElement = this.document.querySelector('html');
+    const position: any = {
+      top: 0,
+      left: 0,
+      x: 0,
+      y: 0
+    };
+
+    const mouseDownHandler = (mouseEvent: MouseEvent): void => {
+      htmlElement.style.cursor = 'row-resize';
+      htmlElement.style.userSelect = 'none';
+
+      position.left = htmlElement.scrollLeft;
+      position.top = htmlElement.scrollTop;
+      position.x = mouseEvent.clientX;
+      position.y = mouseEvent.clientY;
+
+      // prettier-ignore
+      this.mousemove$ = fromEvent(this.document, 'mousemove').subscribe((event: Event) => mouseMoveHandler(event as MouseEvent));
+      this.mouseup$ = fromEvent(this.document, 'mouseup').subscribe(() => mouseUpHandler());
+    };
+
+    const mouseMoveHandler = (mouseEvent: MouseEvent): void => {
+      const dx: number = mouseEvent.clientX - position.x;
+      const dy: number = mouseEvent.clientY - position.y;
+
+      htmlElement.scrollTop = position.top - dy;
+      htmlElement.scrollLeft = position.left - dx;
+    };
+
+    const mouseUpHandler = (): void => {
+      htmlElement.removeAttribute('style');
+
+      this.mousemove$?.unsubscribe();
+      this.mouseup$?.unsubscribe();
+    };
+
+    this.mousedown$ = fromEvent(this.document, 'mousedown')
+      // @ts-ignore
+      .pipe(filter((event: Event) => event.target.parentElement.id === 'grip-horizontal'))
+      .subscribe((event: Event) => mouseDownHandler(event as MouseEvent));
   }
 
   onSubmitPostForm(): void {
