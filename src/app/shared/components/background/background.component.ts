8dcd860ff72e6c9452564abc28ffa3585e21bb5d
denis23x <damage.23x@gmail.com>
2023-02-14 13:47:15 +0800

    daisy: update background

--- src/app/shared/components/background/background.component.ts
+++ src/app/shared/components/background/background.component.ts
@@ -1,16 +1,70 @@
 /** @format */
 
-import { Component, OnInit } from '@angular/core';
-
-/** https://blog.1a23.com/2020/02/13/telegram-chat-backgrounds-patterns-extracted/ */
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SanitizerPipe } from '../../pipes';
+import { AuthService, CookieService, User } from '../../../core';
+import { Subscription } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
 	standalone: true,
 	selector: 'app-background, [appBackground]',
+	imports: [SanitizerPipe],
 	templateUrl: './background.component.html'
 })
-export class BackgroundComponent implements OnInit {
-	constructor() {}
+export class BackgroundComponent implements OnInit, OnDestroy {
+	authUser: User | undefined;
+	authUser$: Subscription | undefined;
 
-	ngOnInit(): void {}
+	background: string | undefined;
+
+	constructor(
+		@Inject(DOCUMENT)
+		private document: Document,
+		private httpClient: HttpClient,
+		private authService: AuthService,
+		private cookieService: CookieService
+	) {}
+
+	ngOnInit(): void {
+		this.authUser$ = this.authService.user.subscribe({
+			next: (user: User) => {
+				this.authUser = user;
+
+				// this.setTheme();
+				this.setBackground();
+			},
+			error: (error: any) => console.error(error)
+		});
+	}
+
+	ngOnDestroy(): void {
+		[this.authUser$].forEach($ => $.unsubscribe());
+	}
+
+	setTheme(): void {
+		const theme: string = this.authUser?.settings.theme || 'light';
+
+		if (!!theme) {
+			this.cookieService.setItem('theme', theme);
+			this.document.documentElement.setAttribute('data-theme', theme);
+		} else {
+			this.cookieService.removeItem('theme');
+			this.document.documentElement.removeAttribute('data-theme');
+		}
+	}
+
+	setBackground(): void {
+		// prettier-ignore
+		const background: string = this.authUser?.settings.background || 'pattern-randomized';
+
+		const url: string = '/assets/backgrounds/' + background + '.svg';
+
+		this.httpClient
+			.get(url, {
+				responseType: 'text'
+			})
+			.subscribe((svg: string) => (this.background = svg));
+	}
 }
