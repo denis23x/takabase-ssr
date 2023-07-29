26a022d0b45113f9ab610c8d40b68d48c24646bc
denis23x <damage.23x@gmail.com>
2023-07-29 23:15:27 +0800

    fix only paste directive

--- src/app/standalone/directives/app-input-only-paste.directive.ts
+++ src/app/standalone/directives/app-input-only-paste.directive.ts
@@ -9,13 +9,14 @@ import { Directive, HostListener } from '@angular/core';
 export class AppInputOnlyPasteDirective {
 	@HostListener('keydown', ['$event']) onKeydown(keyboardEvent: KeyboardEvent) {
 		const ctrlKey: boolean = keyboardEvent.ctrlKey;
+		const metaKey: boolean = keyboardEvent.metaKey;
 
 		const c: boolean = keyboardEvent.key.toLowerCase() === 'c';
 		const v: boolean = keyboardEvent.key.toLowerCase() === 'v';
 
 		const backspace: boolean = keyboardEvent.key.toLowerCase() === 'backspace';
 
-		if (!(ctrlKey && (c || v)) && !backspace) {
+		if (!((ctrlKey || metaKey) && (c || v)) && !backspace) {
 			keyboardEvent.stopImmediatePropagation();
 			keyboardEvent.stopPropagation();
 			keyboardEvent.preventDefault();
