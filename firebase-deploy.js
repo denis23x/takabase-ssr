a5d8598c552f44fa5b47d66cfc4e7f5184ef5634
denis23x <damage.23x@gmail.com>
2024-02-19 20:04:17 +0700

    update-deploy-flow

---
+++ firebase-deploy.js
@@ -0,0 +1,57 @@
+const prompts = require('prompts');
+const spawn = require('child_process').spawn;
+
+(async () => {
+  const select = await prompts({
+    type: 'select',
+    name: 'value',
+    message: 'Select a environment',
+    choices: [
+      {
+        title: 'takabase-dev',
+        value: 'takabase-dev',
+        description: 'https://takabase-dev.web.app',
+      },
+      {
+        title: 'takabase-prod',
+        value: 'takabase-prod',
+        description: 'https://takabase-prod.web.app',
+      },
+    ],
+    initial: 0
+  });
+
+  const confirm = await prompts({
+    type: 'confirm',
+    name: 'value',
+    message: 'Can you confirm?',
+    initial: select.value === 'takabase-dev'
+  });
+
+  if (confirm.value) {
+    const buildTarget = () => {
+      switch (select.value) {
+        case 'takabase-dev': {
+          return 'export FIREBASE_FRAMEWORKS_BUILD_TARGET=\'development\'';
+        }
+        case 'takabase-prod': {
+          return 'export FIREBASE_FRAMEWORKS_BUILD_TARGET=\'production\'';
+        }
+        default: {
+          return 'exit;';
+        }
+      }
+    };
+
+    const command = `${buildTarget()} && firebase use ${select.value} && firebase deploy --only functions,hosting:${select.value}`;
+
+    /** RUN */
+
+    spawn(command, {
+      shell: true,
+      stdio:'inherit'
+    });
+  } else {
+    console.log('Ok, Bye!');
+  }
+})();
