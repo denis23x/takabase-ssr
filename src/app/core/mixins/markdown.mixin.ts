0395003e02500dc1b35d137361f3ee051007e1b3
denis23x <damage.23x@gmail.com>
2024-07-24 13:11:00 +0700

    refactor: update markdown abstracts

---
+++ src/app/core/mixins/markdown.mixin.ts
@@ -0,0 +1,97 @@
+/** @format */
+
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { nanoid } from 'nanoid';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { PlatformService } from '../services/platform.service';
+import { from, Subscription, throwError } from 'rxjs';
+import { catchError, filter, map, switchMap } from 'rxjs/operators';
+
+export function MarkdownMixin<T extends new (...args: any[]) => any>(MasterClass: T) {
+	@Component({
+		selector: 'app-markdown-mixin',
+		template: '',
+		host: {
+			hostID: nanoid()
+		}
+	})
+	abstract class SlaveClass extends MasterClass implements OnInit, OnDestroy {
+		public readonly httpClient: HttpClient = inject(HttpClient);
+		public readonly activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+		public readonly router: Router = inject(Router);
+		public readonly platformService: PlatformService = inject(PlatformService);
+
+		activatedRouteParams$: Subscription | undefined;
+
+		markdownProse: string | undefined;
+		markdownProse$: Subscription | undefined;
+		markdownProseSkeleton: boolean = false;
+
+		ngOnInit(): void {
+			super.ngOnInit && super.ngOnInit();
+
+			// ngOnInit
+
+			this.activatedRouteParams$ = this.activatedRoute.params
+				.pipe(filter((params: Params) => !!params.details))
+				.subscribe({
+					next: () => {
+						/** Apply Data */
+
+						this.setSkeleton();
+						this.setResolver();
+					},
+					error: (error: any) => console.error(error)
+				});
+		}
+
+		ngOnDestroy(): void {
+			super.ngOnDestroy && super.ngOnDestroy();
+
+			// ngOnDestroy
+
+			[this.activatedRouteParams$, this.markdownProse$].forEach(($: Subscription) => $?.unsubscribe());
+		}
+
+		setSkeleton(): void {
+			this.markdownProse = 'Lorem Ipsum';
+			this.markdownProseSkeleton = true;
+		}
+
+		setResolver(): void {
+			const details: string = String(this.activatedRoute.snapshot.paramMap.get('details') || '');
+
+			this.markdownProse$?.unsubscribe();
+			this.markdownProse$ = this.httpClient
+				.get(this.getMarkdownProseUrl(details), {
+					responseType: 'text'
+				})
+				.pipe(
+					map((prose: string) => {
+						const modifierKey: string = this.platformService.getOSModifierKey();
+						const keyboardCharacter: string = this.platformService.getOSKeyboardCharacter(modifierKey);
+						const proseUpdated: string = prose.replaceAll('modifierKey', keyboardCharacter);
+
+						return proseUpdated;
+					}),
+					catchError((httpErrorResponse: HttpErrorResponse) => {
+						return from(this.router.navigate(['/error', httpErrorResponse.status])).pipe(
+							switchMap(() => throwError(() => httpErrorResponse))
+						);
+					})
+				)
+				.subscribe({
+					next: (prose: string) => {
+						this.markdownProse = prose;
+						this.markdownProseSkeleton = false;
+					},
+					error: (error: any) => console.error(error)
+				});
+		}
+
+		abstract getMarkdownProseUrl(markdown: string): string;
+	}
+
+	return SlaveClass;
+}
--- src/app/help/details/details.component.ts
+++ src/app/help/details/details.component.ts
@@ -1,9 +1,9 @@
 /** @format */
 
-import { Component } from '@angular/core';
-import { AbstractMarkdownComponent } from '../../abstracts/abstract-markdown.component';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SkeletonDirective } from '../../standalone/directives/app-skeleton.directive';
 import { MarkdownRenderDirective } from '../../standalone/directives/app-markdown-render.directive';
+import { MarkdownMixin as MD } from '../../core/mixins/markdown.mixin';
 
 @Component({
 	standalone: true,
@@ -11,8 +11,16 @@ import { MarkdownRenderDirective } from '../../standalone/directives/app-markdow
 	selector: 'app-help-details',
 	templateUrl: './details.component.html'
 })
-export class HelpDetailsComponent extends AbstractMarkdownComponent {
-	getAbstractProseUrl(markdown: string): string {
+export class HelpDetailsComponent extends MD(class {}) implements OnInit, OnDestroy {
+	ngOnInit(): void {
+		super.ngOnInit();
+	}
+
+	ngOnDestroy(): void {
+		super.ngOnDestroy();
+	}
+
+	getMarkdownProseUrl(markdown: string): string {
 		return '/assets/markdown/help/' + markdown + '.md';
 	}
 }
